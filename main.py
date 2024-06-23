30b0beec6513b06febf8de17ad1207ac6f793bec
AVNEESH_SINGH <22f3002042@ds.study.iitm.ac.in>
2024-06-23 16:10:02 +0530

    initial commit added file main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+#http://localhost:8000/
+@app.route('/')
+def hello():
+    return '<h1>hello world!</h1>'
+
+if __name__== "__main__":
+    app.run(host= "localhost", port=8000, debug=True)
