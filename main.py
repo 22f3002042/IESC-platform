25234d3061432ea8c0c6333fa74dc92edbad82f4
AVNEESH_SINGH <22f3002042@ds.study.iitm.ac.in>
2024-07-25 01:42:00 +0530

    complete login and signup authentication is done , lillttle bit of tweeks need and now begining towards main component/funcationalities

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from flask import Flask,render_template,\
-request,redirect,url_for,flash
-from sqlalchemy.exc import NoResultFound
+from flask import Flask,render_template,request, redirect, url_for, session, flash
 from db import db
 from models import User, Campaign
 
@@ -13,74 +11,161 @@ db.init_app(app)
 
 
 @app.route("/")
-def index():
+def homepage():
     return render_template("home.html")
 
-# @app.route("/base")
-# def base():
-#     return render_template("base.html")
+@app.route("/base")
+def base():
+    return render_template("base.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def admin_login():
-    if request.method == 'GET':
-        return render_template("/users/login.html")
-    # error = None
-    elif request.method == 'POST':
-        if request.form['email'] != 'admin@gmail.com' or \
-                request.form['password'] != 'admin123':
-            flash('You were enter wrong details','warning')    
-            error = 'Invalid credentials'
-        else:
-            flash('You were successfully logged in','success')
-            return redirect(url_for('admin_dash'))
-    return render_template('users/login.html')
-
-def check_password(user_password, password):
-    return user_password == password
-
-# @app.route("/login" , methods=['GET','POST']) 
-# def login():
+# @app.route('/login', methods=['GET', 'POST'])
+# def admin_login():
 #     if request.method == 'GET':
 #         return render_template("/users/login.html")
-#     elif request.method =='POST':
-#         email = request.form.get("email")
-#         password = request.form.get("password")
-#         print(f'email:{email},password:{password}')
-
-#         try:
-#             user= User.query.filter_by(email=email).one()
-#         except NoResultFound as n:
-#             flash("enter the correct email.", "error")
-            
-#             return redirect(url_for("index"))
-#         authenticated = check_password(user.password ,password)
-#         if authenticated:
-#             return redirect(url_for("index"))
+#     elif request.method == 'POST':
+#         if request.form['email'] != 'admin@gmail.com' or \
+#                 request.form['password'] != 'admin123':
+#             flash('You were enter wrong details','warning')    
+#             error = 'Invalid credentials'
 #         else:
-#             flash("enter correct password.", "error")
-#             return redirect(url_for("login"))
+#             flash('You were successfully logged in','success')
+#             return redirect(url_for('admin_dash'))
+#     return render_template('users/login.html')
 
-@app.route("/sponsor_signup")
+# def check_password(user_password, password):
+#     return user_password == password
+
+
+#new login framwork
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        
+        email = request.form['email']
+        password = request.form['password']
+
+        # print(f'email:{email},password:{password}')
+
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.password == password: 
+            # User authenticated, store user info in session
+            session['user_id'] = user.id
+            session['username'] = user.username 
+            session['user_role'] = user.role
+            session['email'] = user.email  
+            
+            # Redirect based on role
+            if user.role == 'admin':
+                return redirect(url_for('admin_dash'))
+                flash(f'Welcome back Admin- {username}', 'success')
+            elif user.role == 'sponsor':
+                return redirect(url_for('sponsor_dash'))
+                flash(f'Welcome back Sponsor- {username}', 'success')
+            elif user.role == 'influencer':
+                return redirect(url_for('influencer_dash'))
+                flash(f'Welcome back Influencer- {username}', 'success')
+        else:
+            flash('Please Enter correct Email and Passsword', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/sponsor_signup', methods=['GET', 'POST'])
 def sponsor_signup():
-    return render_template("users/sponsor_signup.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        company_name = request.form['company_name']
+        industry = request.form['industry']
+        budget = float(request.form['budget'])
 
-@app.route("/influencer_signup")
+        # Check for email exist
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('This Email account already exist! Try different email.', 'danger')
+            return redirect(url_for('sponsor_signup'))
+        
+        user = User(username=username, email=email, password=password, role='sponsor', company_name=company_name, industry=industry, budget=budget)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Hey {username}  your account created successfully. Please login.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('/sponsor/sponsor_signup.html')
+
+@app.route('/influencer_signup', methods=['GET', 'POST'])
 def influencer_signup():
-    return render_template("users/influencer_signup.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        category = request.form['category']
+        niche = request.form['niche']
+        reach = int(request.form['reach'])
 
-@app.route("/sponsor_dash")
-def sponsor_dash():
-    return render_template("sponsor_dash.html")
+        # Check for email exist
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('This Emails account already exist! Try different email.', 'danger')
+            return redirect(url_for('/influencer/influencer_signup'))
+        
+        user = User(username=username, email=email, password=password, role='influencer', category=category, niche=niche, reach=reach)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Hey {username}  your account created successfully. Please login.', 'success')
+        return redirect(url_for('login'))
 
+    return render_template('/influencer/influencer_signup.html')
 
-@app.route("/influencer_dash")
-def influencer_dash():
-    return render_template("influencer_dash.html")
+#session Logout
+@app.route('/logout')
+def logout():
+    username = session.get('username')
+    session.clear()
+    flash(f'{username} - you have been logged out', 'danger')
+    return redirect(url_for('homepage'))
 
-@app.route("/admin_dash")
+# Admin dashboard route
+@app.route('/admin_dash')
 def admin_dash():
-    return render_template("admin_dash.html")
+    if 'user_role' in session and session['user_role'] == 'admin':
+        username = session.get('username')
+        flash(f'Welcome back Admin - {username} ','success')
+        return render_template('/admin/admin_dash.html',username=username)
+    else:
+        flash('Please log in first!', 'danger')
+        return redirect(url_for('homepage'))
+
+# Sponsor dashboard route
+@app.route('/sponsor_dash')
+def sponsor_dash():
+    if 'user_role' in session and session['user_role'] == 'sponsor':
+        username = session.get('username')
+        flash(f'Welcome back Sponsor - {username} ','success')
+        return render_template('/sponsor/sponsor_dash.html')
+    else:
+        flash('Please log in first!', 'danger')
+        return redirect(url_for('homepage'))
+
+# Influencer dashboard route
+@app.route('/influencer_dash')
+def influencer_dash():
+    if 'user_role' in session and session['user_role'] == 'influencer':
+        username = session.get('username')
+        flash(f'Welcome back Influencer - {username} ','success')
+        return render_template('/influencer/influencer_dash.html')
+    else:
+        flash('Please log in first!', 'danger')
+        return redirect(url_for('homepage'))
+
+
+
+
+
+#######################################################################################################
+
 
 
 
@@ -138,7 +223,7 @@ def delete_user(user_id):
 if __name__ == '__main__':
     with app.app_context():
         # db.create_all()
-        # print(" db is created")
+        # print("db is created")
     
         app.run(debug=True,port=8000)
     
